bdd7168c9634bb831bcdba1b47004aa7a8c9b02c
manu <kllmanu@mailbox.org>
2024-09-18 23:21:35 +0200

    add ability to move rectangles, set corners and get their dimensions

--- src/main/java/at/fhv/mko7395/oop/uebung01/Rectangle.java
+++ src/main/java/at/fhv/mko7395/oop/uebung01/Rectangle.java
@@ -7,13 +7,36 @@ public class Rectangle {
     private Point d;
 
     public Rectangle(Point a, Point c) {
+        this.setCorners(a, c);
+    }
+
+    public Point[] getCorners() {
+        return new Point[]{a, b, c, d};
+    }
+
+    public int getWidth() {
+        return this.c.getX() - this.a.getX();
+    }
+
+    public int getHeight() {
+        return this.c.getY() - this.a.getY();
+    }
+
+    public void setCorners(Point a, Point c) {
         this.a = a;
         this.b = new Point(c.getX(), a.getY());
         this.c = c;
         this.d = new Point(a.getX(), c.getY());
     }
 
-    public Point[] getCorners() {
-        return new Point[]{a, b, c, d};
+    public void setCorners(Point a) {
+        this.a = a;
+        this.b = new Point(a.getX() + this.getWidth(), a.getY());
+        this.c = new Point(a.getX() + this.getWidth(),a.getY() + this.getHeight());
+        this.d = new Point(a.getX(), this.a.getY() + this.getHeight());
+    }
+
+    public void move(Point p) {
+        this.setCorners(p);
     }
 }
--- src/test/java/at/fhv/mko7395/oop/uebung01/RectangleTest.java
+++ src/test/java/at/fhv/mko7395/oop/uebung01/RectangleTest.java
@@ -2,12 +2,11 @@ package at.fhv.mko7395.oop.uebung01;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.*;
 
 import java.util.Arrays;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class RectangleTest {
     Rectangle r1;
@@ -20,8 +19,19 @@ class RectangleTest {
     }
 
     @Test
+    @Order(1)
     void getCorners() {
-        assertEquals(Arrays.toString(r1.getCorners()), "[5,5, 8,5, 8,8, 5,8]");
-        assertEquals(Arrays.toString(r2.getCorners()), "[3,3, 0,3, 0,0, 3,0]");
+        assertEquals("[5,5, 8,5, 8,8, 5,8]", Arrays.toString(r1.getCorners()));
+        assertEquals( "[3,3, 0,3, 0,0, 3,0]", Arrays.toString(r2.getCorners()));
+    }
+
+    @Test
+    @Order(2)
+    void setCorners() {
+        r1.move(new Point(10, 10));
+        r2.move(new Point(-1, -1));
+
+        assertEquals("[10,10, 8,10, 8,8, 10,8]", Arrays.toString(r1.getCorners()));
+        assertEquals("[-1,-1, 0,-1, 0,0, -1,0]", Arrays.toString(r2.getCorners()));
     }
 }
