377c2238057572e9dc035511d64663430a2f759b
manu <kllmanu@mailbox.org>
2024-09-20 01:28:41 +0200

    change grid color

--- src/main/java/at/fhv/mko7395/oop/uebung01/Main.java
+++ src/main/java/at/fhv/mko7395/oop/uebung01/Main.java
@@ -4,7 +4,7 @@ public class Main {
     private static String grid = """
             <defs>
                 <pattern id="grid" width="50" height="50" patternUnits="userSpaceOnUse">
-                    <path d="M 50 0 L 0 0 0 50" fill="none" stroke="gray" stroke-width="1"/>
+                    <path d="M 50 0 L 0 0 0 50" fill="none" stroke="silver" stroke-width="1"/>
                 </pattern>
             </defs>
             <rect width="100%" height="100%" fill="url(#grid)" />""";
